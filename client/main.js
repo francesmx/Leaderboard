227671d21a8e846e1cf6c91b2abaa561cb388244
Frances Maxwell <fmaxwell@gmail.com>
2016-12-05 00:42:15 +0000

    Made a simple Leaderboard application for a single user

--- client/main.js
+++ client/main.js
@@ -1,22 +1,50 @@
+PlayersList = new Mongo.Collection('players');
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.leaderboard.helpers({
+  'player': function(){
+    return PlayersList.find({}, { sort: {score: -1, name: 1} });
+  },
+  'selectedClass': function(){
+    var playerId = this._id;
+    var selectedPlayer = Session.get('selectedPlayer');
+    if(playerId == selectedPlayer){
+      return "selected"
+    }
+  },
+  'selectedPlayer': function(){
+    var selectedPlayer = Session.get('selectedPlayer');
+    return PlayersList.findOne({ _id: selectedPlayer });
+  }
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.leaderboard.events({
+  'click .player': function(){
+    var playerId = this._id;
+    Session.set('selectedPlayer', playerId);
   },
+  'click .increment': function(){
+    var selectedPlayer = Session.get('selectedPlayer');
+    PlayersList.update({ _id: selectedPlayer }, { $inc: {score: 5} });
+  },
+  'click .decrement': function(){
+    var selectedPlayer = Session.get('selectedPlayer');
+    PlayersList.update({ _id: selectedPlayer }, {$inc: {score: -5} });
+  },
+  'click .remove': function(){
+    var selectedPlayer = Session.get('selectedPlayer');
+    PlayersList.remove({ _id: selectedPlayer });
+}
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.addPlayerForm.events({
+  'submit form': function(){
+    event.preventDefault();
+    var playerNameVar = event.target.playerName.value;
+    PlayersList.insert({
+      name: playerNameVar,
+      score: 0
+    });
+    event.target.playerName.value = "";
+  }
 });
